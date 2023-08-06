3e322291a794a1eab87dc7e01a75a60c43b29293
Anuj10031992 <99414617+Anuj10031992@users.noreply.github.com>
2023-08-06 22:36:40 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
   public static void main(String[] args) {
 	
 	  
-	  System.out.println("Welcome to Jenkins tutorials. first commit now again lalu");
+	  System.out.println("Welcome to Jenkins tutorials. first commit now again pransh");
 }
 }
